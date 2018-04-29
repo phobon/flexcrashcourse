23ef892ed58e3e949426fd5311dea4bfdbc323a8
Ben McCormick <exponen7@gmail.com>
2018-04-29 11:03:05 +0800

    Updating to handle styled-components in ssr

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,16 @@
 // ./pages/_document.js
 import Document, { Head, Main, NextScript } from 'next/document'
 import flush from 'styled-jsx/server'
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, ServerStyleSheet } from 'styled-components';
 
 import theme from '../theme';
 
 export default class CustomDocument extends Document {
   static getInitialProps({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage()
-    const styles = flush()
-    return { html, head, errorHtml, chunks, styles }
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
   }
 
   render() {
